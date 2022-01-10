024f48d692f9e2ccc831f35974330ca83dbffef0
Austin-DeYoung <austindeyoung1@gmail.com>
2022-01-09 19:01:09 -0500

    Add files via upload

---
+++ DeYoungAlgorithms/BubbleSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeYoungAlgorithms
+{
+    class BubbleSort
+    {
+        int[] array;
+        int transactionCount = 0;
+
+        public BubbleSort(int[] numbers)
+        {
+            array = numbers;   
+        }
+            
+
+        public int SortContents()
+        {
+            int temp = 0;
+
+            for (int iteration = 0; iteration < array.Length; iteration++)
+            {
+                for (int sort = 0; sort < array.Length - 1; sort++)
+                {
+                    if (array[sort] > array[sort + 1])
+                    {
+                        temp = array[sort + 1];
+                        array[sort + 1] = array[sort];
+                        array[sort] = temp;
+                        transactionCount++;
+                    }
+                }
+            }
+            return transactionCount;
+
+
+
+
+        }
+       
+    }
+}
---
+++ DeYoungAlgorithms/InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeYoungAlgorithms
+{
+    class InsertionSort
+    {
+        int[] array;
+        int transactionCount = 0;
+
+        public InsertionSort(int[] numbers)
+        {
+            array = numbers;
+        }
+
+
+        public int SortContents()
+        {
+            for (int index = 0; index < array.Length - 1; index++)
+            {
+                int value = index + 1;
+
+                while (value > 0)
+                {
+                    if (array[value - 1] > array[value])
+                    {
+                        transactionCount++;
+                        int temp = array[value - 1];
+                        array[value - 1] = array[value];
+                        array[value] = temp;
+
+                    }
+                    value--;
+                }
+            }
+
+
+            return transactionCount;
+
+        }
+    }
+}
